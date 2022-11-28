e818f0952603b351fbfcdd5b144b47cb5608128a
student <dicarrascoc@miners.utep.edu>
2022-11-28 16:24:18 -0700

    undefined reference

--- wakedemo/wakedemo.c
+++ wakedemo/wakedemo.c
@@ -2,7 +2,8 @@
 #include <libTimer.h>
 #include "lcdutils.h"
 #include "lcddraw.h"
-
+#include "buzzer.h"
+#include "sound.h"
 // WARNING: LCD DISPLAY USES P1.0.  Do not touch!!! 
 
 #define LED BIT6		/* note that bit zero req'd for display */
@@ -14,9 +15,13 @@
 
 #define SWITCHES 15
 
+
+int period = 500;
+char sontT= 0;
+/*
 char blue = 31, green = 0, red = 31;
 unsigned char step = 0;
-
+*/
 static char 
 switch_update_interrupt_sense()
 {
@@ -45,37 +50,24 @@ switch_interrupt_handler()
   char p2val = switch_update_interrupt_sense();
   switches = ~p2val & SWITCHES;
 }
-
-
-// axis zero for col, axis 1 for row
-
-short drawPos[2] = {1,10}, controlPos[2] = {2, 10};
-short colVelocity = 1, colLimits[2] = {1, screenWidth/2};
-
-void
-draw_ball(int col, int row, unsigned short color)
-{
-  fillRectangle(col-1, row-1, 3, 3, color);
-}
-
-
+/*1
 void
 screen_update_ball()
 {
   for (char axis = 0; axis < 2; axis ++) 
-    if (drawPos[axis] != controlPos[axis]) /* position changed? */
-      goto redraw;
+    if (drawPixel[axis] != controlPixel[axis]) /* position changed? */
+      /*    goto redraw;2
   return;			/* nothing to do */
- redraw:
+      /* redraw:3
   draw_ball(drawPos[0], drawPos[1], COLOR_BLUE); /* erase */
-  for (char axis = 0; axis < 2; axis ++) 
+/*for (char axis = 0; axis < 2; axis ++) 
     drawPos[axis] = controlPos[axis];
   draw_ball(drawPos[0], drawPos[1], COLOR_WHITE); /* draw */
-}
-  
+
 
 short redrawScreen = 1;
-u_int controlFontColor = COLOR_GREEN;
+
+//u_int controlFontColor = COLOR_GREEN;
 
 void wdt_c_handler()
 {
@@ -83,8 +75,12 @@ void wdt_c_handler()
 
   secCount ++;
   if (secCount >= 25) {		/* 10/sec */
-   
-    {				/* move ball */
+    secCount = 0;
+    redrawScreen =1;
+  }
+}
+    /*
+    {				// move ball 
       short oldCol = controlPos[0];
       short newCol = oldCol + colVelocity;
       if (newCol <= colLimits[0] || newCol >= colLimits[1])
@@ -93,7 +89,7 @@ void wdt_c_handler()
 	controlPos[0] = newCol;
     }
 
-    {				/* update hourglass */
+    {				/* update hourglass 
       if (switches & SW3) green = (green + 1) % 64;
       if (switches & SW2) blue = (blue + 2) % 32;
       if (switches & SW1) red = (red - 3) % 32;
@@ -107,10 +103,10 @@ void wdt_c_handler()
     redrawScreen = 1;
   }
 }
-  
+    */
 void update_shape();
 
-void main()
+int main()
 {
   
   P1DIR |= LED;		/**< Green led on when CPU on */
@@ -132,11 +128,64 @@ void main()
     or_sr(0x10);	/**< CPU OFF */
     P1OUT |= LED;	/* led on */
   }
+  return 0;
 }
 
-void
-screen_update_hourglass()
-{
+unsigned int SHAPE_COLOR = COLOR_DARK_VIOLE;
+
+void update_shape(){
+  
+  static unsigned char row = screenHeight / 2 , col = screenWidth / 2;
+  static int colStep = 5;
+  static int rowStep = 5;
+  static unsigned char step = 0;
+  if(step <= 10)
+    {
+      int startCol = col - step*2;
+      int endCol = col*2 + step*2;
+      int width = 4 + endCol - startCol;
+      
+      //shape drawing, pressing the bottns will chage
+      
+      fillRectangle(endCol, row+1 -step, width, 1, SHAPE_COLOR);
+      fillRectangle(endCol, row+1 +step, width, 1, SHAPE_COLOR);
+      drawPixel(endCol+3, row, SHAPE_COLOR);
+      drawPixel(endCol+3, row-1, SHAPE_COLOR);
+      drawPixel(endCol+3, row+1, SHAPE_COLOR);
+      drawPixel(endCol+3, row-2, SHAPE_COLOR);
+      drawPixel(endCol+3, row+2, SHAPE_COLOR);
+      drawPixel(endCol+3, row-3, SHAPE_COLOR);
+      drawPixel(endCol+3, row+3, SHAPE_COLOR);
+
+      fillRectangle(endCol+30, row*step, width, 1, COLOR_BLACK);
+
+      if(switches & SW1) {
+	SHAPE_COLOR = COLOR_RED;
+	buzzer_set_period(0);
+      }
+      if(switches & SW2){
+	SHAPE_COLOR = COLOR_BLUE;
+	buzzer_set_period(0);
+      }
+      if(switches & SW3){
+	SHAPE_COLOR = COLOR_GREEN;
+	buzzer_set_period(0);
+      }
+      if(switches & SW4){
+	playSong();
+      }
+      step ++;
+
+    }
+  else{
+    col -= colStep;
+    
+    clearScreen(COLOR_BLUE);
+    step =0;
+  }
+}
+  /*
+      screen_update_hourglass(){
   static unsigned char row = screenHeight / 2, col = screenWidth / 2;
   static char lastStep = 0;
   
@@ -156,6 +205,7 @@ screen_update_hourglass()
       fillRectangle(startCol, row-lastStep, width, 1, color);
     }
   }
+      }
 }  
 
 
@@ -168,7 +218,7 @@ update_shape()
 }
    
 
-
+  */
 void
 __interrupt_vec(PORT2_VECTOR) Port_2(){
   if (P2IFG & SWITCHES) {	      /* did a button cause this interrupt? */
